a575e844a05cfe2f47e7ad7b0d150ee8713d757a
nbmblueeye <nbmblueeye@gmail.com>
2024-07-17 10:34:37 -0400

    mongo DB

--- server/database/app.js
+++ server/database/app.js
@@ -15,7 +15,6 @@ mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
 
 const Reviews = require('./review');
-
 const Dealerships = require('./dealership');
 
 try {
@@ -36,6 +35,7 @@ app.get('/', async (req, res) => {
     res.send("Welcome to the Mongoose API")
 });
 
+
 // Express route to fetch all reviews
 app.get('/fetchReviews', async (req, res) => {
   try {
@@ -45,6 +45,7 @@ app.get('/fetchReviews', async (req, res) => {
     res.status(500).json({ error: 'Error fetching documents' });
   }
 });
+
 
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
@@ -56,20 +57,42 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
   }
 });
 
+
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find();
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
+
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find({state: req.params.state});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
+
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find({id: req.params.id});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching documents' });
+    }
 });
+
 
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
@@ -97,6 +120,7 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
     res.status(500).json({ error: 'Error inserting review' });
   }
 });
+
 
 // Start the Express server
 app.listen(port, () => {
